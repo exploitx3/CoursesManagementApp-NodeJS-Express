c0e0e4572ef94b9e3c5c88bbfc9c83fc6f60a3c2
exploitx3 <gogo333x@gmail.com>
2018-03-24 23:40:41 +0000

    clear fix

--- create-release-branches.js
+++ create-release-branches.js
@@ -113,13 +113,7 @@ function getStatusForRunningPipelines(werckerConfig, runningPipelines) {
 }
 
 function clearConsole(werckerConfig) {
-  if (werckerConfig.executedInWercker) {
-
-
-    // fs.writeFileSync(werckerConfig.reportFile, ' ', {encoding: 'utf8', flag: 'w'})
-    process.stdout.write('\u001B[2J\u001B[0;0f')
-
-  } else {
+  if (!werckerConfig.executedInWercker) {
     process.stdout.write('\u001B[2J\u001B[0;0f')
   }
 }
